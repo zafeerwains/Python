730320771299ffe112e5c17a8a617abdafbd5177
Muhammad Zafeer <zafeerwains@gmail.com>
2023-10-21 08:00:29 +0500

    class 2

--- Class-02/class02.py
+++ Class-02/class02.py
@@ -1,1 +1,19 @@
-print("Seocnd class")
+name: str = "Muhammad Zafeer"
+print(type(name))
+print(name)
+
+# convert any special character into simple character by adding \ sign
+message: str = 'His father\'s bussiness is good'
+print(message)
+
+# / line  break to next
+print("this is a \
+good language")
+
+
+# f string power f"""portal{variable}"""
+print(f"""
+Student Name = {name}""")
+
+dir(str)
+[i for i in dir(str) if "__" not in i]
