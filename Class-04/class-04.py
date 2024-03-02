bdd7848dc9a7059b8b86ce1037430fe104af7883
Muhammad Zafeer <zafeerwains@gmail.com>
2024-03-02 12:05:56 +0500

    Chapter 3 Started

---
+++ Class-04/class-04.py
@@ -0,0 +1,18 @@
+# Problem 3-1
+names: list[str] = ["Ali", "Akbar", "Abdullah", "Alam"]
+print(*names)
+
+# Problem 3-2
+print(f"Welcome To the Python Mr.{names[0]}, You can do it .")
+print(f"Welcome To the Python Mr.{names[1]}, You can do it .")
+print(f"Welcome To the Python Mr.{names[2]}, You can do it .")
+print(f"Welcome To the Python Mr.{names[3]}, You can do it .")
+
+# Problem 3-3
+transports: list[str] = ["Car", "Bike", "Plane", "Ship"]
+messages: list[str] = ["I want to drive super", "I want to Drive a Heavy ",
+                       "I want to fly a fighter jet", "I want to sail a "]
+print(f'{messages[0]} {transports[0]}.')
+print(f'{messages[1]} {transports[1]}.')
+print(f'{messages[2]} {transports[2]}.')
+print(f'{messages[3]} {transports[3]}.')
--- class-03/class03.py
+++ class-03/class03.py
@@ -1,9 +1,3 @@
-# name: str = "zafeer"
-# print("q" in name)
-
-# print(10 < 5 and 10 > 5)
-# print(10 < 5 or 10 > 5)
-# print(not (10 < 5 and 10 > 5))
 
 # Problem 2-9
 print(5+3)
@@ -14,4 +8,18 @@ print(16//2)
 print(2**3)
 print(17 % 9)
 
+# Problem 2-10
+fav_number: int = 7
+print(f"My favorite number is {fav_number}")
+
 # Problem 2-11
+# name: str = "zafeer"
+# print("q" in name)
+
+# print(10 < 5 and 10 > 5)
+# print(10 < 5 or 10 > 5)
+# print(not (10 < 5 and 10 > 5))
+
+# Problem 2-12
+import this
+print(this)
