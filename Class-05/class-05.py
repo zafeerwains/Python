76379be9dab3ffc3a15f03840afd873a8493dbda
Muhammad Zafeer <zafeerwains@gmail.com>
2024-03-08 16:00:54 +0500

    Chapter 3 done

---
+++ Class-05/class-05.py
@@ -0,0 +1,17 @@
+# Problem 3-7
+places_to_visit: list[str] = ["Makkah", "Madinah", "Turkey", "Qatar", "Oman"]
+print(places_to_visit)
+print(sorted(places_to_visit))
+print(places_to_visit)
+print(sorted(places_to_visit, reverse=True))
+print(places_to_visit)
+places_to_visit.reverse()
+print(places_to_visit)
+places_to_visit.reverse()
+print(places_to_visit)
+places_to_visit.sort()
+print(places_to_visit)
+places_to_visit.sort(reverse=True)
+print(places_to_visit)
+
+# 3-9 and 3-10  and 3-11 Almost done in prevous questions
