4f0959660fee13c9ff2bd3c21e9b5ab6c34a6ce3
Muhammad Zafeer <zafeerwains@gmail.com>
2024-03-10 19:39:49 +0500

    chapter 4 continue

--- Class-06/class-06.py
+++ Class-06/class-06.py
@@ -12,16 +12,29 @@
 #         print(output)
 
 # Replace "your_input_string" with the actual string you want to process
-s = "qwertyuiopasdfghjklzxcvbnm"
-obj = {}
-# l = []
-for i in s:
-    if i not in obj:
-        obj[i] = 0
-    obj[i] += 1
-values = sorted(obj.items())
-val = sorted(values, key=lambda x: x[-1], reverse=True)[:3]
-print(val[0][0], val[0][1])
-print(val[1][0], val[1][1])
-print(val[2][0], val[2][1])
+# s = "qwertyuiopasdfghjklzxcvbnm"
+# obj = {}
+# # l = []
+# for i in s:
+#     if i not in obj:
+#         obj[i] = 0
+#     obj[i] += 1
+# values = sorted(obj.items())
+# val = sorted(values, key=lambda x: x[-1], reverse=True)[:3]
+# print(val[0][0], val[0][1])
+# print(val[1][0], val[1][1])
+# print(val[2][0], val[2][1])
 
+
+# Chapter 4 Questions strting from here
+# Problem 4-1
+pizzas: list[str] = ["Supremen", "corn", "Kabab"]
+for pizza in pizzas:
+    print(f"The name of the pizza , {pizza}")
+print("The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!")
+
+# Problem 4-2
+Animals: list[str] = ["cat", "lion", "cheetah"]
+for animal in Animals:
+    print(f"I like {animal}")
+print("Any of these animals would make a great pet!")
